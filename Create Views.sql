b83395681e061f1815ac94368f133298fd894257
Peter <57006688+TheOfficialPeter@users.noreply.github.com>
2024-01-23 18:47:32 +0200

    "Added views for total wins and car ownership to the database."

--- Create Views.sql
+++ Create Views.sql
@@ -4,3 +4,10 @@ FROM Player
 LEFT JOIN Round ON Player.PlayerID = Round.WinnerID
 GROUP BY Player.PlayerID
 ORDER BY TotalWins DESC;
+
+CREATE VIEW CarOwnership AS
+SELECT Cars.CarName, COUNT(Inventory.PlayerID) AS TotalOwners
+FROM Cars
+LEFT JOIN Inventory ON Cars.CarID = Inventory.CarID
+GROUP BY Cars.CarID
+ORDER BY TotalOwners DESC;
